1040137a64c053a89fca1e5f4d305cb259b46da8
leii <596878919@qq.com>
2021-11-05 16:08:03 +0800

    【创建项目】

---
+++ testBran/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  testBran
+//
+//  Created by apple on 2021/11/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ testBran/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  testBran
+//
+//  Created by apple on 2021/11/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
